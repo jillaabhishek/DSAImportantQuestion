290dd38b8575f1b66fef74de4beb3152747527de
ABHISHEK\jilla <jilla.abhishek94@gmail.com>
2024-07-10 11:51:30 +0530

    google question

---
+++ Google/KthSmallestElementInSortedMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAImportantQuestion.Google
+{
+    public class KthSmallestElementInSortedMatrix
+    {
+        public void Run()
+        {
+            //int[][] matrix =
+            //        {
+            //            new int[] {1,5,9 },
+            //            new int[] { 10,11,13 },
+            //            new int[] { 12, 13, 15 }
+            //        };
+
+            int[][] matrix =
+                    {
+                        new int[] {1, 2 },
+                        new int[] {1, 3 }
+                    };
+            int k = 2;
+
+            Console.WriteLine(KthSmallest(matrix, k));
+        }
+
+        public int KthSmallest(int[][] matrix, int k)
+        {
+                        
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DSAImportantQuestion.Google;
+
+namespace DSAImportantQuestion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KthSmallestElementInSortedMatrix matrix = new KthSmallestElementInSortedMatrix();
+            matrix.Run();
+        }
+    }
+}
